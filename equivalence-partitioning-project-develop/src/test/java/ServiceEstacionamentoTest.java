41565e2abc4fc2e6a47d0d06e0d268ef91ab518e
leoo9433335 <silvaleonardoduarte2222@gmail.com>
2024-10-06 02:40:58 -0300

    de novo

--- equivalence-partitioning-project-develop/src/test/java/ServiceEstacionamentoTest.java
+++ equivalence-partitioning-project-develop/src/test/java/ServiceEstacionamentoTest.java
@@ -1,10 +1,10 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import org.example.ServiceEstacionamento;
-import org.junit.jupiter.api.Test;
-
 import java.text.SimpleDateFormat;
 import java.util.Date;
+
+import org.example.ServiceEstacionamento;
+import org.junit.jupiter.api.Test;
 
 public class ServiceEstacionamentoTest {
 
